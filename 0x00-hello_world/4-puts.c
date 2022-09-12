317e581ba9fbb5daad0b6501128ab0b0b7db15fb
mikyc <cheruzmiky@gmail.com>
2022-09-12 15:53:40 -0700

    The puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-
+/*Uses puts function to print out strings*/
 int main(void)
 {
 
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+
+	return (0);
 }
