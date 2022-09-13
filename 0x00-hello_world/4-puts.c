3cc828e00df6bf609d2914ea745a08bb00b56df1
mikyc <cheruzmiky@gmail.com>
2022-09-12 18:31:32 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 /* 4-puts.c */
 #include<stdio.h>
- /* The code below prints out string statements using puts function */
-/*
- * main - return type is void
- * returns null
- */
-int main(void) 
+int main(void)
 {
 	/* string output using puts function */
 	char str[] = "\"Programming is like building a multilingual puzzle";
