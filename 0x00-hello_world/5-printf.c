7f026b620165c6e7131c3b9840fb152485ea9cf4
mikyc <cheruzmiky@gmail.com>
2022-09-13 09:06:15 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  *
  * Description: prints string output using printf
  * the code prints with proper grammar, but the outcome is a piece of art,
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 
-	return (0);
+	return (0);/* returns 0 */
 }
