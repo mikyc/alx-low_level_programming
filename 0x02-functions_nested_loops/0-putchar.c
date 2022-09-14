cd70a1d6af6345aecf5bb26cf2a32ed92339b8ea
mikyc <cheruzmiky@gmail.com>
2022-09-14 08:56:48 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints out _putchar using function _putchar 
+ * Return: the program returns zero
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Project on 0x02. C - Functions, nested loops
